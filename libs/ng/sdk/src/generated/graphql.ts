6d96fc97293db6c57478940a1339dc5aaea9e26f
Matthew Coatney <mcoatney@bitwiseindustries.com>
2022-04-20 13:37:12 -0700

    feat(feature-posts.graphql): Adds a query for sdk to consume and produce schema from

---
+++ libs/ng/sdk/src/generated/graphql.ts
@@ -0,0 +1,94 @@
+import { gql } from 'apollo-angular';
+import { Injectable } from '@angular/core';
+import * as Apollo from 'apollo-angular';
+import * as ApolloCore from '@apollo/client/core';
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+  /** A date-time string at UTC, such as 2019-12-03T09:54:33Z, compliant with the date-time format. */
+  DateTime: any;
+};
+
+export type Post = {
+  __typename?: 'Post';
+  author?: Maybe<User>;
+  content?: Maybe<Scalars['String']>;
+  createdAt?: Maybe<Scalars['DateTime']>;
+  id?: Maybe<Scalars['Float']>;
+  title?: Maybe<Scalars['String']>;
+  updatedAt?: Maybe<Scalars['DateTime']>;
+};
+
+export type Query = {
+  __typename?: 'Query';
+  posts?: Maybe<Array<Post>>;
+};
+
+export type User = {
+  __typename?: 'User';
+  createdAt?: Maybe<Scalars['DateTime']>;
+  email?: Maybe<Scalars['String']>;
+  id?: Maybe<Scalars['Float']>;
+  name?: Maybe<Scalars['String']>;
+  updatedAt?: Maybe<Scalars['DateTime']>;
+  username?: Maybe<Scalars['String']>;
+};
+
+export type PostsQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type PostsQuery = { __typename?: 'Query', posts?: Array<{ __typename?: 'Post', id?: number | null, content?: string | null, author?: { __typename?: 'User', id?: number | null, email?: string | null } | null }> | null };
+
+export const PostsDocument = gql`
+    query Posts {
+  posts {
+    id
+    content
+    author {
+      id
+      email
+    }
+  }
+}
+    `;
+
+  @Injectable({
+    providedIn: 'root'
+  })
+  export class PostsGQL extends Apollo.Query<PostsQuery, PostsQueryVariables> {
+    document = PostsDocument;
+    
+    constructor(apollo: Apollo.Apollo) {
+      super(apollo);
+    }
+  }
+
+  type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+
+  interface WatchQueryOptionsAlone<V> extends Omit<ApolloCore.WatchQueryOptions<V>, 'query' | 'variables'> {}
+
+  interface QueryOptionsAlone<V> extends Omit<ApolloCore.QueryOptions<V>, 'query' | 'variables'> {}
+
+  @Injectable({ providedIn: 'root' })
+  export class ApolloAngularSDK {
+    constructor(
+      private postsGql: PostsGQL
+    ) {}
+      
+    posts(variables?: PostsQueryVariables, options?: QueryOptionsAlone<PostsQueryVariables>) {
+      return this.postsGql.fetch(variables, options)
+    }
+    
+    postsWatch(variables?: PostsQueryVariables, options?: WatchQueryOptionsAlone<PostsQueryVariables>) {
+      return this.postsGql.watch(variables, options)
+    }
+  }
