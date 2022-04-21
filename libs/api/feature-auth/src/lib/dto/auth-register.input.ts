6a1bb7e1745cead82ac9a15a9580949c45e25623
Matthew Coatney <mcoatney@bitwiseindustries.com>
2022-04-21 12:11:11 -0700

    feat(auth dto): Adds login and register input dtos  to lib

---
+++ libs/api/feature-auth/src/lib/dto/auth-register.input.ts
@@ -0,0 +1,16 @@
+import { Field, InputType } from '@nestjs/graphql'
+
+@InputType()
+export class AuthRegisterInput {
+  @Field()
+  email: string
+
+  @Field({ nullable: true})
+  password: string
+
+  @Field({ nullable: true})
+  name?: string
+
+  @Field()
+  username?: string
+}
