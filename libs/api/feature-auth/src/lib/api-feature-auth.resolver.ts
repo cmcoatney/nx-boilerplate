5b3dccd0d94809c9e6495bb462b726d0e96b29f1
Matthew Coatney <mcoatney@bitwiseindustries.com>
2022-04-21 12:48:35 -0700

    feat(api-feature-auth.resolver): Updates Mutation and Args parameters

--- libs/api/feature-auth/src/lib/api-feature-auth.resolver.ts
+++ libs/api/feature-auth/src/lib/api-feature-auth.resolver.ts
@@ -3,18 +3,19 @@ import { Args, Mutation, Resolver } from '@nestjs/graphql'
 import { ApiFeatureAuthService } from './api-feature-auth.service'
 import { AuthLoginInput } from './dto/auth-login.input'
 import { AuthRegisterInput } from './dto/auth-register.input'
+import { UserToken } from './models/user-token'
 
 @Resolver()
 export class AuthResolver {
-  constructor(private readonly service: ApiFeatureAuthService){}
+  constructor(private readonly service: ApiFeatureAuthService) {}
 
-  @Mutation()
-  login(@Args('input') input: AuthLoginInput) {
+  @Mutation(() => UserToken)
+  login(@Args({ name: 'input', type: () => AuthLoginInput }) input: AuthLoginInput) {
     return this.service.login(input)
   }
 
-  @Mutation()
-  register(@Args('input') input: AuthRegisterInput) {
+  @Mutation(() => UserToken)
+  register(@Args({ name: 'input', type: () => AuthRegisterInput }) input: AuthRegisterInput) {
     return this.service.register(input)
   }
 }
