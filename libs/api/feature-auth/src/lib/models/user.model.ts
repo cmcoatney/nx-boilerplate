2e610c04590d4122fc13e44a2840e717842aa941
Matthew Coatney <mcoatney@bitwiseindustries.com>
2022-04-21 11:58:17 -0700

    feat(user.model): Adds password to user model and allows optional fields

--- libs/api/feature-auth/src/lib/models/user.model.ts
+++ libs/api/feature-auth/src/lib/models/user.model.ts
@@ -1,22 +1,22 @@
-import {ObjectType, Field } from '@nestjs/graphql'
+import { ObjectType, Field } from '@nestjs/graphql'
 
 // TODO: recommended implement base class
 @ObjectType()
 export class User {
-  @Field({ nullable: true})
+  @Field({ nullable: true })
   id: number
-  @Field({ nullable: true})
+  @Field({ nullable: true })
   createdAt: Date
-  @Field({ nullable: true})
+  @Field({ nullable: true })
   updatedAt: Date
-  @Field({ nullable: true})
-  email: string
-  @Field({ nullable: true})
-  username: string
-  @Field({ nullable: true})
-  name: string | null
+  @Field({ nullable: true })
+  email?: string
+  @Field({ nullable: true })
+  username?: string
+  @Field({ nullable: true })
+  name?: string
   // @Field({ nullable: true})
   // role: Role
+
+  password?: string
 }
-
-
