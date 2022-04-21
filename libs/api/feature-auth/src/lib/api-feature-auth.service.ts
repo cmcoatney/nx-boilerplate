81d021fe522a9d23d2efd778d633caf628cdf0f9
Matthew Coatney <mcoatney@bitwiseindustries.com>
2022-04-21 12:16:12 -0700

    feat(api-feature-auth.service): adds login and register methods to service

--- libs/api/feature-auth/src/lib/api-feature-auth.service.ts
+++ libs/api/feature-auth/src/lib/api-feature-auth.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common'
 
+import { ApiDataAccessService } from '@nxws-data-access';
+import { AuthLoginInput } from './dto/auth-login.input';
+import { AuthRegisterInput } from './dto/auth-register.input';
+
 @Injectable()
-export class ApiFeatureAuthService {}
+export class ApiFeatureAuthService {
+  constructor(private readonly data: ApiDataAccessService) {}
+
+  public login(input: AuthLoginInput): void {}
+
+  public register(input: AuthRegisterInput): void {}
+}
