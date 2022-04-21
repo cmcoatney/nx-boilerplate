f015fa796ddb7d2d12e53f6e890fdc630a6df6df
Matthew Coatney <mcoatney@bitwiseindustries.com>
2022-04-20 22:53:06 -0700

    feat(web/app.module): Imports material lib into frontent

--- apps/web/src/app/app.module.ts
+++ apps/web/src/app/app.module.ts
@@ -1,25 +1,27 @@
 import { NgModule } from '@angular/core'
+import { HttpClientModule } from '@angular/common/http'
 import { BrowserModule } from '@angular/platform-browser'
 
 import { AppComponent } from './app.component'
 import { GraphQLModule } from './graphql.module'
-import { HttpClientModule } from '@angular/common/http'
-
-import { FeaturePostComponentModule } from '@nxws-ng-sdk'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { StoreModule } from '@ngrx/store'
-import { NgStateModule, ngStateRoutes } from '@nxws-ngrx'
 
+import { NgStateModule, ngStateRoutes } from '@nxws-ngrx'
+import { NgMaterialModule } from '@nxws-material'
+
+import { FeaturePostComponentModule } from '@nxws-ng-sdk'
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     GraphQLModule,
     HttpClientModule,
-    FeaturePostComponentModule,
     BrowserAnimationsModule,
     StoreModule.forRoot({}, {}),
     NgStateModule,
+    NgMaterialModule,
+    FeaturePostComponentModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
