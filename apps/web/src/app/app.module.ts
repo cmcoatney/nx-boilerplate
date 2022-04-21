fef413cd85e6d202dc7f6a23b36b6d2b35e7d288
Matthew Coatney <mcoatney@bitwiseindustries.com>
2022-04-20 22:26:04 -0700

    chore(libs): Adds core libs for angular to workspace

--- apps/web/src/app/app.module.ts
+++ apps/web/src/app/app.module.ts
@@ -5,13 +5,22 @@ import { AppComponent } from './app.component'
 import { GraphQLModule } from './graphql.module'
 import { HttpClientModule } from '@angular/common/http'
 
-import { FeaturePostComponentModule } from '@nxws-ng-sdk';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FeaturePostComponentModule } from '@nxws-ng-sdk'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { StoreModule } from '@ngrx/store'
+import { NgStateModule, ngStateRoutes } from '@nxws-ngrx'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, GraphQLModule, HttpClientModule, FeaturePostComponentModule, BrowserAnimationsModule, StoreModule.forRoot({}, {})],
+  imports: [
+    BrowserModule,
+    GraphQLModule,
+    HttpClientModule,
+    FeaturePostComponentModule,
+    BrowserAnimationsModule,
+    StoreModule.forRoot({}, {}),
+    NgStateModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
