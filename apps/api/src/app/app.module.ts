b9fa6d222c87d08b13a2219b9bff1279396125fb
Matthew Coatney <mcoatney@bitwiseindustries.com>
2022-04-21 13:48:27 -0700

    feat(config): Adds joi .env configuration through core api module

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -7,11 +7,13 @@ import { AppService } from './app.service'
 
 import { ApiFeaturePostModule } from '@nxws-data-access/post'
 import { ApiFeatureAuthModule } from '@nxws-data-access/auth'
+import { ApiCoreModule } from '@nxws-api/core' //'libs/api/core/src/lib/api-core.module'
+
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({ driver: ApolloDriver, autoSchemaFile: true }),
-    
+    ApiCoreModule,
     ApiFeaturePostModule,
     ApiFeatureAuthModule,
 
