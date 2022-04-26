62a38dbf63014276e6889f546548bf93d85c0647
Matthew Coatney <mcoatney@bitwiseindustries.com>
2022-04-26 10:13:11 -0700

    feat(api-feature-auth.service): Implements password validation and hashing

--- libs/api/feature-auth/src/lib/api-feature-auth.service.ts
+++ libs/api/feature-auth/src/lib/api-feature-auth.service.ts
@@ -1,6 +1,7 @@
 import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common'
 
 import { ApiDataAccessService } from '@nxws-data-access'
+import { AuthHelper } from '@nxws/shared/util'
 import { AuthLoginInput } from './dto/auth-login.input'
 import { AuthRegisterInput } from './dto/auth-register.input'
 import { UserToken } from './models/user-token'
@@ -16,7 +17,7 @@ export class ApiFeatureAuthService {
       throw new NotFoundException(`User with email ${input.email} does not exist`)
     }
 
-    const passwordValid = await this.validate(input.password, found.password)
+    const passwordValid = await AuthHelper.validate(input.password, found.password)
 
     if (!passwordValid) {
       throw new Error(`Invalid password`)
@@ -30,9 +31,14 @@ export class ApiFeatureAuthService {
     if (found) {
       throw new BadRequestException(`Cannot register with email ${input.email}`)
     }
+    const hashedPassword = await AuthHelper.hash(input.password)
 
     const created = await this.data.user.create({
-      data: { email: input.email, username: input.username, password: input.password },
+      data: {
+        email: input.email,
+        username: input.username,
+        password: hashedPassword,
+      },
     })
 
     return { user: created, token: this.signToken(created.id) }
@@ -40,9 +46,5 @@ export class ApiFeatureAuthService {
 
   signToken(id: number): string {
     return ' TEMP TOKEN FOR ID ' + id
-  }
-
-  private async validate(password: string, password2: any): Promise<boolean> {
-    return false
   }
 }
