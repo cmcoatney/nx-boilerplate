4c8f3ee3886cf4087c7d948df3fb23c99949dad9
Matthew Coatney <mcoatney@bitwiseindustries.com>
2022-04-26 09:45:26 -0700

    feat(AuthHelper): Adds auth utility class to shared utility lib

--- libs/shared/util/src/lib/shared-util.ts
+++ libs/shared/util/src/lib/shared-util.ts
@@ -1,3 +1,12 @@
 export function sharedUtil(): string {
   return 'shared-util'
 }
+
+export class AuthHelper {
+  static validate(): Promise<boolean> {
+    //
+  }
+  static hash(password: string): Promise<string> {
+    //
+  }
+}
